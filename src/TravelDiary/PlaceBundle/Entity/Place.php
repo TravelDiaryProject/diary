e31dfab34edcb53ad71513c8bbf5c374294d3b1c
Pavel <ppecheny@i.ua>
2015-01-22 23:46:53 +0200

    exif test

--- src/TravelDiary/PlaceBundle/Entity/Place.php
+++ src/TravelDiary/PlaceBundle/Entity/Place.php
@@ -306,14 +306,14 @@ class Place
         // if there is an error when moving the file, an exception will
         // be automatically thrown by move(). This will properly prevent
         // the entity from being persisted to the database on error
-        $reader = \PHPExif\Reader::factory(\PHPExif\Reader::TYPE_NATIVE);
+        //$reader = \PHPExif\Reader::factory(\PHPExif\Reader::TYPE_NATIVE);
 
         // reader with Exiftool adapter
         //$reader = \PHPExif\Reader::factory(\PHPExif\Reader::TYPE_EXIFTOOL);
 
-        $exif = $reader->getExifFromFile($this->getFile()->getPathname());
+        //$exif = $reader->getExifFromFile($this->getFile()->getPathname());
 
-        print_r($exif);
+        print_r($this->read_gps_location($this->getFile()->getPathname()));
         die();
 
 
@@ -330,6 +330,47 @@ class Place
             $this->temp = null;
         }
         $this->file = null;
+    }
+
+    public function read_gps_location($file){
+        if (is_file($file)) {
+            $info = exif_read_data($file);
+            if (isset($info['GPSLatitude']) && isset($info['GPSLongitude']) &&
+                isset($info['GPSLatitudeRef']) && isset($info['GPSLongitudeRef']) &&
+                in_array($info['GPSLatitudeRef'], array('E','W','N','S')) && in_array($info['GPSLongitudeRef'], array('E','W','N','S'))) {
+
+                $GPSLatitudeRef  = strtolower(trim($info['GPSLatitudeRef']));
+                $GPSLongitudeRef = strtolower(trim($info['GPSLongitudeRef']));
+
+                $lat_degrees_a = explode('/',$info['GPSLatitude'][0]);
+                $lat_minutes_a = explode('/',$info['GPSLatitude'][1]);
+                $lat_seconds_a = explode('/',$info['GPSLatitude'][2]);
+                $lng_degrees_a = explode('/',$info['GPSLongitude'][0]);
+                $lng_minutes_a = explode('/',$info['GPSLongitude'][1]);
+                $lng_seconds_a = explode('/',$info['GPSLongitude'][2]);
+
+                $lat_degrees = $lat_degrees_a[0] / $lat_degrees_a[1];
+                $lat_minutes = $lat_minutes_a[0] / $lat_minutes_a[1];
+                $lat_seconds = $lat_seconds_a[0] / $lat_seconds_a[1];
+                $lng_degrees = $lng_degrees_a[0] / $lng_degrees_a[1];
+                $lng_minutes = $lng_minutes_a[0] / $lng_minutes_a[1];
+                $lng_seconds = $lng_seconds_a[0] / $lng_seconds_a[1];
+
+                $lat = (float) $lat_degrees+((($lat_minutes*60)+($lat_seconds))/3600);
+                $lng = (float) $lng_degrees+((($lng_minutes*60)+($lng_seconds))/3600);
+
+                //If the latitude is South, make it negative.
+                //If the longitude is west, make it negative
+                $GPSLatitudeRef  == 's' ? $lat *= -1 : '';
+                $GPSLongitudeRef == 'w' ? $lng *= -1 : '';
+
+                return array(
+                    'lat' => $lat,
+                    'lng' => $lng
+                );
+            }
+        }
+        return false;
     }
 
     private $temp;
