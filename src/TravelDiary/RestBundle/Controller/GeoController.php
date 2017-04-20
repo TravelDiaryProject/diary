0a02b6e6bf9c340d625468f445c6ebdaecdffc36
pavel.pechenyi <pavel.pechenyi@tonicfortealth.com>
2017-04-20 20:40:16 +0300

    countries list

--- src/TravelDiary/RestBundle/Controller/GeoController.php
+++ src/TravelDiary/RestBundle/Controller/GeoController.php
@@ -8,6 +8,7 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use TravelDiary\GeoBundle\Entity\City;
+use TravelDiary\GeoBundle\Entity\Country;
 
 class GeoController extends FOSRestController
 {
@@ -33,6 +34,44 @@ class GeoController extends FOSRestController
         } else {
             /** @var City[] $data */
             $data = $em->getRepository('TDGeoBundle:City')->findAll();
+        }
+
+        $result = array();
+
+        foreach ($data as $entity) {
+            $result[] = array(
+                'id'    => $entity->getId(),
+                'name' => $entity->getName(),
+            );
+        }
+
+        $view = $this->view($result, 200);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Rest\Get("/countries")
+     *
+     * @return JsonResponse
+     */
+    public function getAllCountriesAction(Request $request)
+    {
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+
+        $search = $request->query->get('search');
+
+        if ('' !== $search) {
+            $data = $em->getRepository('TDGeoBundle:Country')
+                ->createQueryBuilder('country')
+                ->where('country.name LIKE :search')
+                ->setParameter('search', $search.'%')
+                ->getQuery()
+                ->getResult();
+        } else {
+            /** @var Country[] $data */
+            $data = $em->getRepository('TDGeoBundle:Country')->findAll();
         }
 
         $result = array();
